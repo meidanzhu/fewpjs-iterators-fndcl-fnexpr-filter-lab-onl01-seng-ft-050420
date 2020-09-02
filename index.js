fe9781662aff91e5366cbef2ae8fa10ac0db63a7
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-09-02 05:13:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(driver => {return driver.toLowerCase() === string.toLowerCase()})
+}
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(driver => {return driver.toLowerCase().startsWith(string.toLowerCase())})
+}
+function matchName(drivers, string) {
+  return drivers.filter(driver => {return driver.name.toLowerCase() === string.toLowerCase()})
+}

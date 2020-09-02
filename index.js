496fd7c80babcd696b88b85be62dca6901c5d6ff
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-09-02 05:15:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(driver => {
+    return driver.toLowerCase() === name.toLowerCase()
+  })
+}
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(driver => {
+    return driver.toLowerCase().startsWith(name.toLowerCase())
+  })
+}
+function matchName(drivers, name) {
+  return drivers.filter(driver => {
+    return driver.name.toLowerCase() === name.toLowerCase()
+  })
+}
